e9b17fdd83bbfb20a0fda143a277fdbd65280480
Lucia <luciaandreeaursu@yahoo.com>
2024-10-31 13:22:21 +0200

    create class to connect to our database

---
+++ src/main/java/com/itfactory/PhoneBookProject/controllers/ContactController.java
@@ -0,0 +1,12 @@
+package com.itfactory.PhoneBookProject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactController {
+    @GetMapping("/contacts")
+    public String getAllContacts(){
+        return  "contacts";
+    }
+}
