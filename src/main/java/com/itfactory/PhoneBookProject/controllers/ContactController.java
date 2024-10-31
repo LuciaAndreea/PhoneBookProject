c1942b410da9f4cd71e08871e21a86f4913f90bd
Lucia <luciaandreeaursu@yahoo.com>
2024-10-31 14:24:17 +0200

    calls the function from the ContactRepository class where is the connection to database

--- src/main/java/com/itfactory/PhoneBookProject/controllers/ContactController.java
+++ src/main/java/com/itfactory/PhoneBookProject/controllers/ContactController.java
@@ -1,6 +1,8 @@
 package com.itfactory.PhoneBookProject.controllers;
 
 import com.itfactory.PhoneBookProject.model.Contact;
+import com.itfactory.PhoneBookProject.repos.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,29 +13,16 @@ import java.util.List;
 
 @Controller
 public class ContactController {
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/phone_book";
-    private static final String DB_USER = "postgres";
-    private static final String DB_PASS = "Lucia5255688665";
+    private ContactRepository contactRepo;
+
+    @Autowired
+    public void setContactRepo(ContactRepository contactRepo) {
+        this.contactRepo = contactRepo;
+    }
+
     @GetMapping("/contacts")
     public String getAllContacts(Model myModel){
-        List<Contact> contactList = new ArrayList<>();
-        try(Connection connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASS)){
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM contacts");
-            while(resultSet.next()){
-                Contact newContact = new Contact();
-                newContact.setId(Integer.parseInt(resultSet.getString("id")));
-                newContact.setName(resultSet.getString("name"));
-                newContact.setSurname(resultSet.getString("surname"));
-                newContact.setPhoneNumber(resultSet.getString("phoneNumber"));
-                newContact.setEmail(resultSet.getString("email"));
-
-                contactList.add(newContact);
-            }
-        }catch (SQLException e){
-            System.out.println("Connection failure");
-            e.printStackTrace();
-        }
+        List<Contact> contactList = contactRepo.getAllContacts();
         myModel.addAttribute("contacte", contactList);
         return  "contacts";
     }
