cbf5a6bb2010d83a3d5d22368579bc74e0e0c864
Lucia <luciaandreeaursu@yahoo.com>
2024-10-31 14:01:04 +0200

    connect to my phone_book database

--- src/main/java/com/itfactory/PhoneBookProject/controllers/ContactController.java
+++ src/main/java/com/itfactory/PhoneBookProject/controllers/ContactController.java
@@ -1,12 +1,40 @@
 package com.itfactory.PhoneBookProject.controllers;
 
+import com.itfactory.PhoneBookProject.model.Contact;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ContactController {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/phone_book";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASS = "Lucia5255688665";
     @GetMapping("/contacts")
-    public String getAllContacts(){
+    public String getAllContacts(Model myModel){
+        List<Contact> contactList = new ArrayList<>();
+        try(Connection connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASS)){
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM contacts");
+            while(resultSet.next()){
+                Contact newContact = new Contact();
+                newContact.setId(Integer.parseInt(resultSet.getString("id")));
+                newContact.setName(resultSet.getString("name"));
+                newContact.setSurname(resultSet.getString("surname"));
+                newContact.setPhoneNumber(resultSet.getString("phoneNumber"));
+                newContact.setEmail(resultSet.getString("email"));
+
+                contactList.add(newContact);
+            }
+        }catch (SQLException e){
+            System.out.println("Connection failure");
+            e.printStackTrace();
+        }
+        myModel.addAttribute("contacte", contactList);
         return  "contacts";
     }
 }
