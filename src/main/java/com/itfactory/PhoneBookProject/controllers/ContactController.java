0d758f9029e0b80ccefafb6ee9179066a8b86101
Lucia <luciaandreeaursu@yahoo.com>
2024-11-01 11:26:58 +0200

    add a spring method RedirectView

--- src/main/java/com/itfactory/PhoneBookProject/controllers/ContactController.java
+++ src/main/java/com/itfactory/PhoneBookProject/controllers/ContactController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -25,5 +27,12 @@ public class ContactController {
         List<Contact> contactList = contactRepo.getAllContacts();
         myModel.addAttribute("contacte", contactList);
         return  "contacts";
+    }
+
+    @PostMapping("/contacts")
+    public RedirectView addNewContact(Contact newContact){
+        contactRepo.addContact(newContact);
+
+        return new RedirectView("http://localhost/contacts");
     }
 }
